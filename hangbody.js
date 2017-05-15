1f2cb697a0a7cbfa9c7f0f082a46dea070e52a93
Heather Arvidson <heatherarvidson@Heathers-MacBook-Pro.local>
2017-05-14 19:30:11 -0700

    Language edits

--- hangbody.js
+++ hangbody.js
@@ -241,7 +241,7 @@ var guess;
 
 function getGuess() {
   if (wrongGuessesCount > 0) {
-    guess = prompt(`What letter do you want to guess? (Your word so far: ${answerArray.join("   ")}. You've already guessed the following letters: ${lettersGuessed.join(", ")}.) <p>If you want to guess the whole word, or quit the game, click cancel.</p>`);
+    guess = prompt(`What letter do you want to guess? (Your word so far: ${answerArray.join("   ")}. You've already guessed the following letters: ${lettersGuessed.join(", ")}.) If you want to guess the whole word, or quit the game, click cancel.`);
   } else {
     guess = prompt(`What letter do you want to guess? (Your word so far: ${answerArray.join("   ")}.) If you want to guess the whole word, or quit the game, click cancel.`);
   }
